bc07a04a2cbcfa2fa7fb754ddfccf55a658d5f81
xgro <zero1401@naver.com>
2022-06-24 22:08:56 +0900

    myFirst terraform aws instance

---
+++ main.tf
@@ -0,0 +1,29 @@
+terraform {
+  # cloud {
+  #   organization = "<ORG_NAME>"
+  #   workspaces {
+  #     name = "Example-Workspace"
+  #   }
+  # }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "ap-northeast-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-058165de3b7202099"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = var.instance_name
+  }
+}
